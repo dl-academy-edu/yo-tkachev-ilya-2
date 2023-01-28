96ff063db179e45248a72c3af01aac509594dd9d
Iliya Tkachev <xarament@gmail.com>
2023-01-28 14:00:27 +0400

    added close to esc

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 const popup = document.querySelector(".popup");
 const openBtn = document.querySelector(".banner__btn-js");
-const closeBtn = document.querySelector(".popup")
+const closeBtn = document.querySelector(".popup-img")
+const focustInput = document.querySelector(".contactUs__input")
+
 
 openBtn.addEventListener("click", function()  {
     popup.classList.add("open")
+    focustInput.focus()
 })
 
 closeBtn.addEventListener("click", function()  {
     popup.classList.remove("open")
+})
+
+window.addEventListener("keydown", function(e) {
+    if(e.code === "Escape" && popup.classList.contains("open")){
+        popup.classList.remove("open")
+    }
 })
